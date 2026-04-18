db9b1c5d82124aba8848623721c0474880af8be9
Princeton Afeez <princetonafeez@gmail.com>
2026-04-18 14:34:09 -0700

    facility.adapters - Imports and small helpers bridging subsystems

---
+++ facility/adapters.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from datetime import UTC, date, datetime, time
+from decimal import Decimal
+
+from .bootstrap import ensure_subproject_paths
+
+ensure_subproject_paths()
+
